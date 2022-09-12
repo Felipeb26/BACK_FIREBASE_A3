dab4d42ccd5bd18e38993c3c7bed559fabd3f1b5
felipe.silva <felipe.silva@grupomultiplica.com.br>
2022-09-12 16:54:14 -0300

    deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const cron = require("./src/utils/timer_delete")
 
 const port = process.env.HOST_PORT;
 const app = express();
@@ -10,4 +11,7 @@ app.use(cors());
 const userRoute = require("./src/routes/user.routes")
 app.use(userRoute)
 
-app.listen(port, () => console.log(`Aplicação rodando normalmente na porta: ${port}`));
+app.listen(port, () => {
+    console.log(`Aplicação rodando normalmente na porta: ${port}`);
+    cron.run();
+});
