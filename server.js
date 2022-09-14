9b6ace862fc8ec4353733e851a7eb67d0182d433
felipe.silva <felipe.silva@grupomultiplica.com.br>
2022-09-14 15:05:03 -0300

    deploy

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const cron = require("./src/utils/timer_delete")
 process.setMaxListeners(0)
 
 const port = process.env.HOST_PORT;
+const host = process.env.HOST_NAME;
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -13,7 +15,7 @@ app.use(cors());
 const userRoute = require("./src/routes/user.routes")
 app.use(userRoute)
 
-app.listen(port,() => {
-    console.log(`Aplicação rodando normalmente na porta: ${port}`);
+app.listen(port,host,() => {
+    console.log(`Aplicação rodando normalmente na url:http://${host}:${port}`);
     cron.run();
 });
